16eb2b5f85d06d03d613f13822c31170cb9025a1
CarrotNuts <kain6245@gmail.com>
2021-11-17 22:31:12 +0900

    Add onLogin

--- src/view/Login.jsx
+++ src/view/Login.jsx
@@ -1,16 +1,51 @@
+import { useState, useEffect } from "react";
 import React from "react";
 
 function Login() {
 
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+
+	useEffect(() => {
+		
+		return () => {
+
+		}
+	}, [])
+
+	const onLogin = () => {
+		window.fetch("http://localhost:8080/api/users/v1/login", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({"username":username, "password":password})
+		})
+		.then(res => res.json())
+		.then(data => console.log(data))
+	}
 
 	return (
 	<div>
+		<h1>Login</h1>
 		<div id="id_pw_wrap">
 			<div className="input_row" id="id_line">
-				<input type="text" placeholder="아이디"/>
+				<input 
+					id="username"
+					type="text" 
+					placeholder="아이디" 
+					value={username}
+					onChange={(e) => setUsername(e.target.value)}
+					/>
 			</div>
 			<div className="input_row" id="pw_line">
-				<input type="password" placeholder="비밀번호" />
+				<input
+					id="password" 
+					type="password" 
+					placeholder="비밀번호" 
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+					/>
 			</div>
 		</div>
 		<div>
@@ -18,7 +53,7 @@ function Login() {
 			<label for="keep">로그인 상태 유지</label>
 		</div>
 		<div>
-			<button>로그인</button>
+			<button onClick={onLogin}>로그인</button>
 		</div>
 		<ui>
 			<li>
