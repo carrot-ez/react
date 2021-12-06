a094f3c0527b1214b739cecf11acca2abe95e93b
CarrotNuts <kain6245@gmail.com>
2021-12-06 21:43:04 +0900

    add react-redux, redux/toolkit

---
+++ src/store/user/UserSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const UserSlice = createSlice({
+	name: 'user',
+	initialState: {
+		userInfo: null,
+	},
+	reducers: {
+		setUserInfo: (state, action) => {
+			state.userInfo = action.payload;
+		},
+	},
+})
+
+export const { setUserInfo } = UserSlice.actions;
+
+export default UserSlice.reducer;
